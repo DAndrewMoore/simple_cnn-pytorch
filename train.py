74ec44aa9c92bc779d32ad46885d56eb616e702d
Daniel Andrew Moore <d.andrew.moo@gmail.com>
2018-11-27 18:16:58 -0500

    removed extraneous code, changed test/train progress display

--- train.py
+++ train.py
@@ -25,52 +25,15 @@ num_chan = 1
 l_r = 1e-4
 batch_size = 16
 num_epochs = 500
+num_cpu = 4
 ########################
 
-t_p, t_n, f_p, f_n = [0] * 4
-def resetGlobalCounts():
-    global t_p, t_n, f_p, f_n
-    t_p, t_n, f_p, f_n = [0] * 4
-
-def updateCounts(pred, lbls):
-    global t_p, t_n, f_p, f_n
-    pred = np.round(pred.detach().cpu().numpy())
-    lbls = lbls.data.cpu().numpy()
-    for cur_btch in range(batch_size):
-        cur_btch_lbls = lbls[cur_btch]
-        cur_btch_pred = pred[cur_btch]
-        for cur_idx in range(num_class):
-            if cur_btch_lbls[cur_idx] == cur_btch_pred[cur_idx]:
-                if cur_btch_lbls[cur_idx] == 1:
-                    t_p += 1
-                else:
-                    t_n += 1
-            elif cur_btch_lbls[cur_idx] == 1:
-                f_n += 1
-            else:
-                f_p += 1
-
-def calcGlobalF1():
-    global t_p, t_n, f_p, f_n
-    f1_msr = (2 * t_p) / (2 * t_p + f_n + f_p)
-    resetGlobalCounts()
-    return f1_msr
-
 def save_epoch(model, save_dir, epoch_num, cur_loss):
-    torch.save(model, os.path.join(save_dir, str(epoch_num)+'.model'))
-    target = open(os.path.join(save_dir, 'loss_list.tsv'), 'w+')
+    torch.save(model.state_dict(), os.path.join(save_dir, str(epoch_num)+'.model'))
+    target = open(os.path.join(save_dir, 'loss_list.tsv'), 'a')
     strTime = '_'.join([time.strftime('%Y-%m-%d'), time.strftime('%H:%M:%S')])
     target.write('%s\t%d\t%.3f\n' % (strTime, epoch_num, cur_loss))
     target.close()
-
-def calc_precision(tp, fp):
-    return tp / (tp + fp)
-
-def calc_recall(tp, fn):
-    return tp / (tp + fn)
-
-def calc_f1(tp, tn, fp, fn):
-    return (2 * tp) / (2 * tp + fn + fp)
 
 def main():
     # Set up data loader
@@ -79,10 +42,10 @@ def main():
     train, validate = torch.utils.data.random_split(dataset, (train_size, len(dataset) - train_size))
     tr_loader = data.DataLoader(dataset=train,
                                 batch_size=batch_size,
-                                shuffle=True, num_workers=4)
+                                shuffle=True, num_workers=num_cpu)
     te_loader = data.DataLoader(dataset=validate,
                                 batch_size=batch_size,
-                                shuffle=False,num_workers=4)
+                                shuffle=False,num_workers=num_cpu)
     tr_btch_interval = len(tr_loader) // 4
     te_btch_interval = len(te_loader) // 4
     # Set up the classifier
@@ -93,7 +56,8 @@ def main():
     optim = torch.optim.Adam(mdl.parameters(), lr=l_r)
     for epoch in range(num_epochs):
         # Creates a training set iterator
-        print('[%03d] Training...\t' % epoch, end=' ', flush=True)
+        print('[%03d] Training...\t' % epoch, end='', flush=True)
+        run_loss = 0
         train_iter = iter(tr_loader)
         for idx, dat in enumerate(train_iter):
             pics = dat[0].to(device).float()
@@ -101,14 +65,15 @@ def main():
             out = mdl(pics)
             optim.zero_grad()
             loss = loss_func(out, clss)
+            run_loss += loss.item()
             loss.backward()
             optim.step()
             if (idx + 1) % tr_btch_interval == 0:
                 cur_per = ((idx +1) // tr_btch_interval) * 25
-                print('%3d%%' % (cur_per) , end=' ', flush=True)
-        # save_epoch(mdl, os.path.join(save_dir), epoch, -1) # tot_loss / tot_test)
+                print('%4d%%' % (cur_per) , end='', flush=True)
+        print('\t%.5f' % (run_loss / len(tr_loader)))
         # Creates a test set iterator
-        print('\n[%03d] Validating...\t' % epoch, end=' ', flush=True)
+        print('[%03d] Validating...\t' % epoch, end='', flush=True)
         run_loss = 0
         valid_iter = iter(te_loader)
         with torch.no_grad():
@@ -119,10 +84,10 @@ def main():
                 run_loss += F.binary_cross_entropy_with_logits(out, clss).item()
                 if (idx + 1) % te_btch_interval == 0:
                     cur_per = ((idx +1) // te_btch_interval) * 25
-                    print('%3d%%' % (cur_per), end=' ', flush=True)
-        print('\n[%03d] :: %.5f' % (epoch, run_loss / len(te_loader)))
+                    print('%4d%%' % (cur_per), end='', flush=True)
+        print('\t%.5f' % (run_loss / len(te_loader)))
+        save_epoch(mdl, os.path.join(save_dir), epoch, run_loss / len(te_loader)) # tot_loss / tot_test)
     torch.save(mdl, finl_sve)
-
 
 if __name__ == '__main__':
     main()
